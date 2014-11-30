8d4bc90966ca044e895a541e13bd6c26f72c4ea7
brucelee2000 <brucelee2000@tp.edu.tw>
2014-11-29 19:47:00 -0800

    Add animation during transition.

--- LionsAndTigers/ViewController.swift
+++ LionsAndTigers/ViewController.swift
@@ -9,17 +9,78 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var myImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var ageLabel: UILabel!
+    @IBOutlet weak var breedLabel: UILabel!
+    
+    var myTigers:[Tiger] = []
+    var currentIndex = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
+        var myTiger = Tiger()
+        myTiger.name = "Tigger"
+        myTiger.age = 3
+        myTiger.breed = "Bengel"
+        myTiger.image = UIImage(named: "Tiger2.jpeg")
+        
+        myTigers.append(myTiger)
+        println("My tiger's name is : \(myTiger.name) and its breed is \(myTiger.breed) and the image is \(myTiger.image)")
+        
+        myImageView.image = myTiger.image
+        nameLabel.text = myTiger.name
+        ageLabel.text = "\(myTiger.age)"
+        breedLabel.text = "\(myTiger.breed)"
+        
+        currentIndex = 0
+        
+        var secondTiger = Tiger()
+        secondTiger.name = "Tigress"
+        secondTiger.breed = "Indochinese Tiger"
+        secondTiger.age = 2
+        secondTiger.image = UIImage(named: "Tiger1.jpeg")
+        
+        var thirdTiger = Tiger()
+        thirdTiger.name = "Jacob"
+        thirdTiger.breed = "Malayam Tiger"
+        thirdTiger.age = 3
+        thirdTiger.image = UIImage(named: "Tiger3.jpeg")
+        
+        var fourthTiger = Tiger()
+        fourthTiger.name = "Spar"
+        fourthTiger.breed = "Siberian Tiger"
+        fourthTiger.age = 5
+        fourthTiger.image = UIImage(named: "Tiger4.jpeg")
+        
+        myTigers += [secondTiger, thirdTiger, fourthTiger]
+}
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func nextBarButtonItemPressed(sender: UIBarButtonItem) {
+        var randomIndex = Int(arc4random_uniform(UInt32(myTigers.count)))
+        while currentIndex == randomIndex {
+            randomIndex = Int(arc4random_uniform(UInt32(myTigers.count)))
+        }
+        currentIndex = randomIndex
+        let tiger = myTigers[randomIndex]
+        
+        // Add animation for transition
+        UIView.transitionWithView(self.view, duration: 1, options: UIViewAnimationOptions.TransitionCrossDissolve, animations: {
+            
+            self.myImageView.image = tiger.image
+            self.nameLabel.text = tiger.name
+            self.ageLabel.text = "\(tiger.age)"
+            self.breedLabel.text = tiger.breed
+            
+            }, completion: { (finished:Bool) -> () in })
+    }
 
 }
 
